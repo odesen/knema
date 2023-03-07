37ccda1ac78a3981e596846e5ca22c954f2b9fd3
odesen <odesen@users.noreply.github.com>
2023-03-07 23:05:55 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func logging(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path)
+		f(w, r)
+	}
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+}
+
+func main() {
+	http.HandleFunc("/", logging(foo))
+
+	http.ListenAndServe(":8000", nil)
+}
